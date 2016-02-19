811dd18b3870d074f131bc33eb6f077490a398ca
Bryan Abbe <bryan.abbe@armedia.com>
2016-02-19 17:42:47 -0500

    First commit

---
+++ src/com/armedia/mvc/dto/User.java
@@ -0,0 +1,31 @@
+package com.armedia.mvc.dto;
+
+public class User {
+	private Integer id;
+	private String name;
+	
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+}
