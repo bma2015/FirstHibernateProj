90dacfd7be7fd2db5b5a2923b497cb50180dd2f8
Bryan Abbe <bryan.abbe@armedia.com>
2016-04-29 15:26:54 -0400

    Updated with list

--- src/com/armedia/mvc/demo/HibernateDemo.java
+++ src/com/armedia/mvc/demo/HibernateDemo.java
@@ -6,26 +6,80 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.armedia.mvc.dto.Address;
 import com.armedia.mvc.dto.MyUser;
 
 public class HibernateDemo {
 
 	public static void main(String[] args) {
 		MyUser user = new MyUser();
-		user.setId(2);
-		user.setName("Second user");
-		user.setAddress("Second user's address");
+		MyUser user2 = new MyUser();
+
+		Address addr = new Address();
+		addr.setStreet("Street Name");
+		addr.setCity("City Name");
+		addr.setState("State Name");
+		addr.setPincode("Pincode");
+		
+		Address addr2 = new Address();
+		addr2.setStreet("Second Street Name");
+		addr2.setCity("Second City Name");
+		addr2.setState("Second State Name");
+		addr2.setPincode("Second Pincode");
+		
+		user.getListOfAddresses().add(addr);
+		user.getListOfAddresses().add(addr2);
+		
+		user.setName("First user");
+		user.setHomeAddress(addr);
+		user.setOfficeAddress(addr2);
 		user.setJoinedDate(new Date());
 		user.setDescription("Description of the user goes here");
+		
+		user2.setName("Second user");
+		user2.setHomeAddress(addr);
+		user2.setOfficeAddress(addr2);
+		user2.setJoinedDate(new Date());
+		user2.setDescription("Description of the user goes here");
+
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
 
 		try {
-			SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-			Session session = sessionFactory.openSession();
 			session.beginTransaction();
 			session.save(user);
+			session.save(user2);
 			session.getTransaction().commit();
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+
+//		user = null;
+//		session = sessionFactory.openSession();
+//
+//		try {
+//			session.beginTransaction();
+//			user = (MyUser) session.get(MyUser.class, 2);
+//			System.out.println("User name retrieved is " + user.getName());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		} finally {
+//			session.close();
+//		}
+		
+		user = null;
+		session = sessionFactory.openSession();
+
+		try {
+			session.beginTransaction();
+			user = (MyUser) session.get(MyUser.class, 1);
+			System.out.println("User name retrieved is " + user.getListOfAddresses());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
 		}
 	}
 }
--- src/com/armedia/mvc/dto/MyUser.java
+++ src/com/armedia/mvc/dto/MyUser.java
@@ -1,92 +1,176 @@
 package com.armedia.mvc.dto;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
 import javax.persistence.Basic;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
 @Entity
-@Table (name = "MY_USER")
+@Table(name = "MY_USER")
 public class MyUser {
+
+	// @EmbeddedId
+	// private LoginName userId;
+
 	@Id
-	@Column (name = "user_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "user_id")
 	private Integer id;
-	
-	@Transient // Ignore field
+
+	// @Transient // Ignore field
 	private String name;
-	
-	@Temporal (TemporalType.DATE)
+
+	@ElementCollection
+	@JoinTable(name = "user_address", joinColumns = @JoinColumn(name = "user_id") )
+	@GenericGenerator(name = "hilo-gen", strategy = "hilo")
+	@CollectionId(columns = { @Column(name = "address_id") }, generator = "hilo-gen", type = @Type(type = "long") )
+	private Collection<Address> listOfAddresses = new ArrayList<Address>();
+
+	@Temporal(TemporalType.DATE)
 	private Date joinedDate;
 
-	@Basic
-	private String address;
-	
+	// @Basic
+	@Embedded
+	@AttributeOverrides({ @AttributeOverride(name = "street", column = @Column(name = "home_street_name") ),
+			@AttributeOverride(name = "city", column = @Column(name = "home_city_name") ),
+			@AttributeOverride(name = "state", column = @Column(name = "home_state_name") ),
+			@AttributeOverride(name = "pincode", column = @Column(name = "home_pin_code") ) })
+	private Address homeAddress;
+
+	@Embedded
+	@AttributeOverrides({ @AttributeOverride(name = "street", column = @Column(name = "office_street_name") ),
+			@AttributeOverride(name = "city", column = @Column(name = "office_city_name") ),
+			@AttributeOverride(name = "state", column = @Column(name = "office_state_name") ),
+			@AttributeOverride(name = "pincode", column = @Column(name = "office_pin_code") ) })
+	private Address officeAddress;
+
 	@Lob // Large object
 	private String description;
-	
+
 	/**
 	 * @return the id
 	 */
 	public Integer getId() {
 		return id;
 	}
+
 	/**
-	 * @param id the id to set
+	 * @param id
+	 *            the id to set
 	 */
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	/**
 	 * @return the name
 	 */
 	public String getName() {
 		return name;
 	}
+
 	/**
-	 * @param name the name to set
+	 * @param name
+	 *            the name to set
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	/**
+	 * @return the listOfAddresses
+	 */
+	public Collection<Address> getListOfAddresses() {
+		return listOfAddresses;
+	}
+
+	/**
+	 * @param listOfAddresses
+	 *            the listOfAddresses to set
+	 */
+	public void setListOfAddresses(Collection<Address> listOfAddresses) {
+		this.listOfAddresses = listOfAddresses;
+	}
+
 	/**
 	 * @return the joinedDate
 	 */
 	public Date getJoinedDate() {
 		return joinedDate;
 	}
+
 	/**
-	 * @param joinedDate the joinedDate to set
+	 * @param joinedDate
+	 *            the joinedDate to set
 	 */
 	public void setJoinedDate(Date joinedDate) {
 		this.joinedDate = joinedDate;
 	}
+
 	/**
-	 * @return the address
+	 * @return the homeAddress
 	 */
-	public String getAddress() {
-		return address;
+	public Address getHomeAddress() {
+		return homeAddress;
 	}
+
 	/**
-	 * @param address the address to set
+	 * @param homeAddress
+	 *            the homeAddress to set
 	 */
-	public void setAddress(String address) {
-		this.address = address;
+	public void setHomeAddress(Address homeAddress) {
+		this.homeAddress = homeAddress;
 	}
+
+	/**
+	 * @return the officeAddress
+	 */
+	public Address getOfficeAddress() {
+		return officeAddress;
+	}
+
+	/**
+	 * @param officeAddress
+	 *            the officeAddress to set
+	 */
+	public void setOfficeAddress(Address officeAddress) {
+		this.officeAddress = officeAddress;
+	}
+
 	/**
 	 * @return the description
 	 */
 	public String getDescription() {
 		return description;
 	}
+
 	/**
-	 * @param description the description to set
+	 * @param description
+	 *            the description to set
 	 */
 	public void setDescription(String description) {
 		this.description = description;
