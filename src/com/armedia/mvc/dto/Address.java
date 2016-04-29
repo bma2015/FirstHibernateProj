bc1f4fd07f3a09eb12b35f5c9db6701bd3b90340
Bryan Abbe <bryan.abbe@armedia.com>
2016-04-29 15:27:26 -0400

    Added Address class

---
+++ src/com/armedia/mvc/dto/Address.java
@@ -0,0 +1,65 @@
+package com.armedia.mvc.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+	@Column (name = "street_name")
+	private String street;
+	@Column (name = "city_name")
+	private String city;
+	@Column (name = "state_name")
+	private String state;
+	private String pincode;
+	
+	/**
+	 * @return the street
+	 */
+	public String getStreet() {
+		return street;
+	}
+	/**
+	 * @param street the street to set
+	 */
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+	/**
+	 * @return the pincode
+	 */
+	public String getPincode() {
+		return pincode;
+	}
+	/**
+	 * @param pincode the pincode to set
+	 */
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+
+}
